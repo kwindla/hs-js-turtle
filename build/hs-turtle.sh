bb514db63997d3f7718c49d5e1e804628a83d4f3
Kwindla Hultman Kramer <kwindla@gmail.com>
2014-04-30 15:55:50 -0700

    and now with a cabal project file

--- build/hs-turtle.sh
+++
@@ -1,6 +0,0 @@
-BUILDIR=$(dirname $0)
-SRCDIR=${BUILDIR}/../hs-core
-MAIN=${SRCDIR}/Main.hs
-
-#ghc ${BASEDIR}/../hs-core/Main.hs -outputdir $BASEDIR -o turtle
-ghc ${MAIN} -i${SRCDIR} -outputdir $BUILDIR/intermediate-files -o turtle
---
+++ tests/test.hs
@@ -0,0 +1,10 @@
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import TurtlePrimitives
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "tests" []
