04a0dca184707fe03c291b21546db43dbf488ec2
Kwindla Hultman Kramer <kwindla@gmail.com>
2014-05-17 18:39:44 -0700

    Pushing new bundle that incorporates indexed colors with nice interpolation. Try this:
      K(1/8)i=-1^M5 5^#10{#10{Ji=i+1 X10 10^B10 0^}^B(-100)10^}

--- js/app-bundle.js
+++ js/app-bundle.js
@@ -544,7 +544,7 @@ console.log ("ready steady")
 // colors list is from the svg documentation.
 // http://www.w3.org/TR/SVG/types.html#ColorKeywords
 
-var namedColorsLookupTable
+var colorsTable = require ('./colors')
 
 function Color (r, g, b, a) {
   var d = 255/99
@@ -560,8 +560,10 @@ function Color (r, g, b, a) {
       , toStringNoAlpha: function () {
           return 'rgb(' + [this.r, this.g, this.b].
                     map(function(c){return Math.round(c)}).join(',') + ')' }
-      , setFromIndex: function (idx) {
-        return namedColorsLookupTable[idx][1](this)
+      , setFromIndex: function (idx) {        
+        var c = colorsTable [idx]
+        if (!c) { dbg ("no color found for", idx); return }
+        this.setRGBFromBytes (c.r, c.g, c.b)
       }
     }
   )
@@ -574,163 +576,12 @@ function Color (r, g, b, a) {
 }
 
 
-var rgb = function (R, G, B) {
-  return function (color) {color.setRGBFromBytes(R,G,B)}
-}
-
-namedColorsLookupTable =
-  [ ['aliceblue', rgb(240, 248, 255)]
-  , ['antiquewhite', rgb(250, 235, 215)]
-  , ['aqua', rgb( 0, 255, 255)]
-  , ['aquamarine', rgb(127, 255, 212)]
-  , ['azure', rgb(240, 255, 255)]
-  , ['beige', rgb(245, 245, 220)]
-  , ['bisque', rgb(255, 228, 196)]
-  , ['black', rgb( 0, 0, 0)]
-  , ['blanchedalmond', rgb(255, 235, 205)]
-  , ['blue', rgb( 0, 0, 255)]
-  , ['blueviolet', rgb(138, 43, 226)]
-  , ['brown', rgb(165, 42, 42)]
-  , ['burlywood', rgb(222, 184, 135)]
-  , ['cadetblue', rgb( 95, 158, 160)]
-  , ['chartreuse', rgb(127, 255, 0)]
-  , ['chocolate', rgb(210, 105, 30)]
-  , ['coral', rgb(255, 127, 80)]
-  , ['cornflowerblue', rgb(100, 149, 237)]
-  , ['cornsilk', rgb(255, 248, 220)]
-  , ['crimson', rgb(220, 20, 60)]
-  , ['cyan', rgb( 0, 255, 255)]
-  , ['darkblue', rgb( 0, 0, 139)]
-  , ['darkcyan', rgb( 0, 139, 139)]
-  , ['darkgoldenrod', rgb(184, 134, 11)]
-  , ['darkgray', rgb(169, 169, 169)]
-  , ['darkgreen', rgb( 0, 100, 0)]
-  , ['darkgrey', rgb(169, 169, 169)]
-  , ['darkkhaki', rgb(189, 183, 107)]
-  , ['darkmagenta', rgb(139, 0, 139)]
-  , ['darkolivegreen', rgb( 85, 107, 47)]
-  , ['darkorange', rgb(255, 140, 0)]
-  , ['darkorchid', rgb(153, 50, 204)]
-  , ['darkred', rgb(139, 0, 0)]
-  , ['darksalmon', rgb(233, 150, 122)]
-  , ['darkseagreen', rgb(143, 188, 143)]
-  , ['darkslateblue', rgb( 72, 61, 139)]
-  , ['darkslategray', rgb( 47, 79, 79)]
-  , ['darkslategrey', rgb( 47, 79, 79)]
-  , ['darkturquoise', rgb( 0, 206, 209)]
-  , ['darkviolet', rgb(148, 0, 211)]
-  , ['deeppink', rgb(255, 20, 147)]
-  , ['deepskyblue', rgb( 0, 191, 255)]
-  , ['dimgray', rgb(105, 105, 105)]
-  , ['dimgrey', rgb(105, 105, 105)]
-  , ['dodgerblue', rgb( 30, 144, 255)]
-  , ['firebrick', rgb(178, 34, 34)]
-  , ['floralwhite', rgb(255, 250, 240)]
-  , ['forestgreen', rgb( 34, 139, 34)]
-  , ['fuchsia', rgb(255, 0, 255)]
-  , ['gainsboro', rgb(220, 220, 220)]
-  , ['ghostwhite', rgb(248, 248, 255)]
-  , ['gold', rgb(255, 215, 0)]
-  , ['goldenrod', rgb(218, 165, 32)]
-  , ['grey', rgb(128, 128, 128)]
-  , ['green', rgb( 0, 128, 0)]
-  , ['greenyellow', rgb(173, 255, 47)]
-  , ['honeydew', rgb(240, 255, 240)]
-  , ['hotpink', rgb(255, 105, 180)]
-  , ['indianred', rgb(205, 92, 92)]
-  , ['indigo', rgb( 75, 0, 130)]
-  , ['ivory', rgb(255, 255, 240)]
-  , ['khaki', rgb(240, 230, 140)]
-  , ['lavender', rgb(230, 230, 250)]
-  , ['lavenderblush', rgb(255, 240, 245)]
-  , ['lawngreen', rgb(124, 252, 0)]
-  , ['lemonchiffon', rgb(255, 250, 205)]
-  , ['lightblue', rgb(173, 216, 230)]
-  , ['lightcoral', rgb(240, 128, 128)]
-  , ['lightcyan', rgb(224, 255, 255)]
-  , ['lightgoldenrodyellow', rgb(250, 250, 210)]
-  , ['lightgray', rgb(211, 211, 211)]
-  , ['lightgreen', rgb(144, 238, 144)]
-  , ['lightgrey', rgb(211, 211, 211)]
-  , ['lightpink', rgb(255, 182, 193)]
-  , ['lightsalmon', rgb(255, 160, 122)]
-  , ['lightseagreen', rgb( 32, 178, 170)]
-  , ['lightskyblue', rgb(135, 206, 250)]
-  , ['lightslategray', rgb(119, 136, 153)]
-  , ['lightslategrey', rgb(119, 136, 153)]
-  , ['lightsteelblue', rgb(176, 196, 222)]
-  , ['lightyellow', rgb(255, 255, 224)]
-  , ['lime', rgb( 0, 255, 0)]
-  , ['limegreen', rgb( 50, 205, 50)]
-  , ['linen', rgb(250, 240, 230)]
-  , ['magenta', rgb(255, 0, 255)]
-  , ['maroon', rgb(128, 0, 0)]
-  , ['mediumaquamarine', rgb(102, 205, 170)]
-  , ['mediumblue', rgb( 0, 0, 205)]
-  , ['mediumorchid', rgb(186, 85, 211)]
-  , ['mediumpurple', rgb(147, 112, 219)]
-  , ['mediumseagreen', rgb( 60, 179, 113)]
-  , ['mediumslateblue', rgb(123, 104, 238)]
-  , ['mediumspringgreen', rgb( 0, 250, 154)]
-  , ['mediumturquoise', rgb( 72, 209, 204)]
-  , ['mediumvioletred', rgb(199, 21, 133)]
-  , ['midnightblue', rgb( 25, 25, 112)]
-  , ['mintcream', rgb(245, 255, 250)]
-  , ['mistyrose', rgb(255, 228, 225)]
-  , ['moccasin', rgb(255, 228, 181)]
-  , ['navajowhite', rgb(255, 222, 173)]
-  , ['navy', rgb( 0, 0, 128)]
-  , ['oldlace', rgb(253, 245, 230)]
-  , ['olive', rgb(128, 128, 0)]
-  , ['olivedrab', rgb(107, 142, 35)]
-  , ['orange', rgb(255, 165, 0)]
-  , ['orangered', rgb(255, 69, 0)]
-  , ['orchid', rgb(218, 112, 214)]
-  , ['palegoldenrod', rgb(238, 232, 170)]
-  , ['palegreen', rgb(152, 251, 152)]
-  , ['paleturquoise', rgb(175, 238, 238)]
-  , ['palevioletred', rgb(219, 112, 147)]
-  , ['papayawhip', rgb(255, 239, 213)]
-  , ['peachpuff', rgb(255, 218, 185)]
-  , ['peru', rgb(205, 133, 63)]
-  , ['pink', rgb(255, 192, 203)]
-  , ['plum', rgb(221, 160, 221)]
-  , ['powderblue', rgb(176, 224, 230)]
-  , ['purple', rgb(128, 0, 128)]
-  , ['red', rgb(255, 0, 0)]
-  , ['rosybrown', rgb(188, 143, 143)]
-  , ['royalblue', rgb( 65, 105, 225)]
-  , ['saddlebrown', rgb(139, 69, 19)]
-  , ['salmon', rgb(250, 128, 114)]
-  , ['sandybrown', rgb(244, 164, 96)]
-  , ['seagreen', rgb( 46, 139, 87)]
-  , ['seashell', rgb(255, 245, 238)]
-  , ['sienna', rgb(160, 82, 45)]
-  , ['silver', rgb(192, 192, 192)]
-  , ['skyblue', rgb(135, 206, 235)]
-  , ['slateblue', rgb(106, 90, 205)]
-  , ['slategray', rgb(112, 128, 144)]
-  , ['slategrey', rgb(112, 128, 144)]
-  , ['snow', rgb(255, 250, 250)]
-  , ['springgreen', rgb( 0, 255, 127)]
-  , ['steelblue', rgb( 70, 130, 180)]
-  , ['tan', rgb(210, 180, 140)]
-  , ['teal', rgb( 0, 128, 128)]
-  , ['thistle', rgb(216, 191, 216)]
-  , ['tomato', rgb(255, 99, 71)]
-  , ['turquoise', rgb( 64, 224, 208)]
-  , ['violet', rgb(238, 130, 238)]
-  , ['wheat', rgb(245, 222, 179)]
-  , ['white', rgb(255, 255, 255)]
-  , ['whitesmoke', rgb(245, 245, 245)]
-  , ['yellow', rgb(255, 255, 0)]
-  , ['yellowgreen', rgb(154, 205, 50)]
-  ]
-
-
 module.exports = Color
 
-},{}],7:[function(require,module,exports){
+
+function dbg () { console.log.apply(null, arguments) }
+
+},{"./colors":13}],7:[function(require,module,exports){
 
 var _st = require ('./SymbolTable')
 
@@ -1392,7 +1243,7 @@ function _derivedTable () {
 
 function dbg (s) { console.log(s) }
 
-},{"util":16}],10:[function(require,module,exports){
+},{"util":17}],10:[function(require,module,exports){
 
 exports.tokenize = tokenize
 
@@ -1865,6 +1716,109 @@ function SVGElementFromProgramState (s, width, height) {
 }
 
 },{"./Evaluator":7,"./Parser":8,"./Tokenizer":10,"./TurtlePrimitives":11}],13:[function(require,module,exports){
+module.exports =
+[ { r: 0, g: 0, b: 0 },
+  { r: 21, g: 21, b: 21 },
+  { r: 34, g: 34, b: 34 },
+  { r: 48, g: 48, b: 48 },
+  { r: 63, g: 63, b: 63 },
+  { r: 78, g: 78, b: 78 },
+  { r: 94, g: 94, b: 94 },
+  { r: 111, g: 111, b: 111 },
+  { r: 127, g: 127, b: 127 },
+  { r: 145, g: 145, b: 145 },
+  { r: 162, g: 162, b: 162 },
+  { r: 180, g: 180, b: 180 },
+  { r: 198, g: 198, b: 198 },
+  { r: 217, g: 217, b: 217 },
+  { r: 236, g: 236, b: 236 },
+  { r: 255, g: 255, b: 255 },
+  { r: 255, g: 207, b: 191 },
+  { r: 255, g: 158, b: 129 },
+  { r: 255, g: 104, b: 70 },
+  { r: 255, g: 0, b: 0 },
+  { r: 187, g: 25, b: 9 },
+  { r: 122, g: 27, b: 12 },
+  { r: 63, g: 21, b: 8 },
+  { r: 255, g: 223, b: 197 },
+  { r: 255, g: 192, b: 140 },
+  { r: 255, g: 160, b: 82 },
+  { r: 255, g: 128, b: 0 },
+  { r: 186, g: 95, b: 15 },
+  { r: 121, g: 64, b: 18 },
+  { r: 62, g: 35, b: 14 },
+  { r: 255, g: 255, b: 207 },
+  { r: 255, g: 255, b: 158 },
+  { r: 255, g: 255, b: 103 },
+  { r: 255, g: 255, b: 0 },
+  { r: 185, g: 185, b: 25 },
+  { r: 120, g: 119, b: 27 },
+  { r: 61, g: 59, b: 21 },
+  { r: 231, g: 255, b: 206 },
+  { r: 203, g: 255, b: 155 },
+  { r: 170, g: 255, b: 101 },
+  { r: 128, g: 255, b: 0 },
+  { r: 97, g: 185, b: 23 },
+  { r: 67, g: 119, b: 26 },
+  { r: 38, g: 60, b: 20 },
+  { r: 219, g: 255, b: 205 },
+  { r: 177, g: 255, b: 154 },
+  { r: 125, g: 255, b: 100 },
+  { r: 0, g: 255, b: 0 },
+  { r: 34, g: 185, b: 23 },
+  { r: 36, g: 119, b: 25 },
+  { r: 27, g: 60, b: 20 },
+  { r: 216, g: 255, b: 224 },
+  { r: 173, g: 255, b: 192 },
+  { r: 121, g: 255, b: 160 },
+  { r: 0, g: 255, b: 128 },
+  { r: 33, g: 185, b: 95 },
+  { r: 34, g: 119, b: 64 },
+  { r: 26, g: 60, b: 35 },
+  { r: 219, g: 255, b: 255 },
+  { r: 178, g: 255, b: 255 },
+  { r: 126, g: 255, b: 255 },
+  { r: 0, g: 255, b: 255 },
+  { r: 35, g: 185, b: 185 },
+  { r: 36, g: 119, b: 119 },
+  { r: 27, g: 60, b: 59 },
+  { r: 215, g: 221, b: 255 },
+  { r: 171, g: 189, b: 255 },
+  { r: 118, g: 157, b: 255 },
+  { r: 0, g: 128, b: 255 },
+  { r: 31, g: 95, b: 185 },
+  { r: 33, g: 63, b: 119 },
+  { r: 25, g: 34, b: 60 },
+  { r: 220, g: 196, b: 255 },
+  { r: 179, g: 139, b: 255 },
+  { r: 126, g: 82, b: 255 },
+  { r: 0, g: 0, b: 255 },
+  { r: 35, g: 14, b: 185 },
+  { r: 36, g: 17, b: 120 },
+  { r: 27, g: 15, b: 60 },
+  { r: 232, g: 201, b: 255 },
+  { r: 204, g: 148, b: 255 },
+  { r: 171, g: 92, b: 255 },
+  { r: 128, g: 0, b: 255 },
+  { r: 97, g: 19, b: 185 },
+  { r: 67, g: 22, b: 119 },
+  { r: 38, g: 18, b: 60 },
+  { r: 255, g: 211, b: 255 },
+  { r: 255, g: 165, b: 255 },
+  { r: 255, g: 112, b: 255 },
+  { r: 255, g: 0, b: 255 },
+  { r: 185, g: 28, b: 185 },
+  { r: 120, g: 30, b: 119 },
+  { r: 61, g: 23, b: 60 },
+  { r: 255, g: 209, b: 222 },
+  { r: 255, g: 160, b: 189 },
+  { r: 255, g: 106, b: 158 },
+  { r: 255, g: 0, b: 128 },
+  { r: 186, g: 26, b: 95 },
+  { r: 121, g: 28, b: 63 },
+  { r: 62, g: 22, b: 34 } ]
+
+},{}],14:[function(require,module,exports){
 if (typeof Object.create === 'function') {
   // implementation from standard node.js 'util' module
   module.exports = function inherits(ctor, superCtor) {
@@ -1889,7 +1843,7 @@ if (typeof Object.create === 'function') {
   }
 }
 
-},{}],14:[function(require,module,exports){
+},{}],15:[function(require,module,exports){
 // shim for using process in browser
 
 var process = module.exports = {};
@@ -1954,14 +1908,14 @@ process.chdir = function (dir) {
     throw new Error('process.chdir is not supported');
 };
 
-},{}],15:[function(require,module,exports){
+},{}],16:[function(require,module,exports){
 module.exports = function isBuffer(arg) {
   return arg && typeof arg === 'object'
     && typeof arg.copy === 'function'
     && typeof arg.fill === 'function'
     && typeof arg.readUInt8 === 'function';
 }
-},{}],16:[function(require,module,exports){
+},{}],17:[function(require,module,exports){
 (function (process,global){
 // Copyright Joyent, Inc. and other Node contributors.
 //
@@ -2551,4 +2505,4 @@ function hasOwnProperty(obj, prop) {
 }
 
 }).call(this,require("K/m7xv"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./support/isBuffer":15,"K/m7xv":14,"inherits":13}]},{},[5])
+},{"./support/isBuffer":16,"K/m7xv":15,"inherits":14}]},{},[5])
